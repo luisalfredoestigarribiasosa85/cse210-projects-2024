a6533dd080e9d8a58fa98334f8f0c0413b7a59e7
luisalfredoestigarribiasosa85 <luisalfredoestigarribiasosa85@gmail.com>
2024-09-23 22:36:23 -0400

    finished Learning02, Prepare

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._companyName = "THN";
+        job1._jobTitle = "Driver";
+        job1._startYear = 2023;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._companyName = "The embassy of Qatar";
+        job2._jobTitle = "Diplomatic Driver";
+        job2._startYear = 2024;
+        job2._endYear = 2024;
+
+        Resume resume = new Resume();
+        resume._name = "Luis Estigarribia";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.DisplayResume();
     }
 }
