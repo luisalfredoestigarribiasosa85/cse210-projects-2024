2f5a3c0d7c7fa8cdfca2804f8f084ea900ada49f
luisalfredoestigarribiasosa85 <luisalfredoestigarribiasosa85@gmail.com>
2024-10-15 22:26:44 -0300

    Finished Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment assignment1 = new Assignment("Luis", "Travel to the center of the earth");
+
+        string showDetails = assignment1.GetSummary();
+
+        Console.WriteLine(showDetails);
+
+        MathAssignment mathAssignment1 = new MathAssignment("Alfredo", "Math in the real world", "5.2", "8-20");
+        string showSummary = mathAssignment1.GetSummary();
+        string showHomeworkList = mathAssignment1.GetHomeworkList();
+
+        Console.WriteLine(showSummary);
+        Console.WriteLine(showHomeworkList);
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Karina", "World War II", "Helping people after war");
+        string showWritingSummary = writingAssignment1.GetSummary();
+        string showWritingInfo = writingAssignment1.GetWritingInformation();
+
+        Console.WriteLine(showWritingSummary);
+        Console.WriteLine(showWritingInfo);
+
     }
 }
